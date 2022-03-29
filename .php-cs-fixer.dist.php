ac60d651de0c4b4a9af75b9885c7afa122d0e7c7
VojtaB <brozek@thepay.cz>
2022-03-29 18:05:28 +0200

    php-cs-fixer config improvement

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -6,11 +6,13 @@ return (new \PhpCsFixer\Config())
     ->setRules([
         '@PSR12' => true,
         '@PSR1' => true,
+        'no_unused_imports' => true,
     ])
     ->setFinder(
         \PhpCsFixer\Finder::create()
             ->in(__DIR__.'/config')
             ->in(__DIR__.'/src')
+            ->in(__DIR__.'/test')
     )
     ->setUsingCache(false)
 ;
